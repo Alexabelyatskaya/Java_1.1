12e49f9991f4aadbefb08fbba45c64c1e216e442
Alexandra <alexabelyatskaya@yandex.ru>
2021-09-29 14:31:19 +0300

    Task1

---
+++ Task1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 2_000_000_000;
+        int receipt = 500_000_000;
+        int total = account + receipt;
+        System.out.println(total);
+    }
+}
